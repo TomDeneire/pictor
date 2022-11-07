857ebc796e6397c3c917084d7cbefd57a87f59f1
Tom Deneire <tomdeneire@gmail.com>
2022-11-07 14:46:43 +0100

    optimized sparql query

--- data/harvester/getty.py
+++ data/harvester/getty.py
@@ -2,11 +2,13 @@ import requests
 
 SPARQL = "https://data.getty.edu/museum/collection/sparql"
 QUERY = """PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
-        SELECT ?iiif WHERE {?item crm:P129i_is_subject_of ?iiif}"""
+SELECT ?iiif WHERE {
+  ?item crm:P129i_is_subject_of ?iiif .
+  FILTER(CONTAINS(STR(?iiif),'iiif/manifest'))
+}"""
 
 result = requests.get(SPARQL, params={"format": "json", "query": QUERY}).json()
 
 for item in result["results"]["bindings"]:
     link = item["iiif"]["value"]
-    if "iiif/manifest" in link:
-        print(link)
+    print(link)
