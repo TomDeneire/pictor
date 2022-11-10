a2025185510f7198ad40410ecdd8c693129bbe1e
Tom Deneire <tomdeneire@gmail.com>
2022-11-10 11:53:43 +0100

    better SPARQL for Getty

--- data/harvester/getty.py
+++ data/harvester/getty.py
@@ -2,9 +2,10 @@ import requests
 
 SPARQL = "https://data.getty.edu/museum/collection/sparql"
 QUERY = """PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 SELECT ?iiif WHERE {
   ?item crm:P129i_is_subject_of ?iiif .
-  FILTER(CONTAINS(STR(?iiif),'iiif/manifest'))
+  ?iiif rdfs:label "IIIF Manifest URL" .
 }"""
 
 result = requests.get(SPARQL, params={"format": "json", "query": QUERY}).json()
